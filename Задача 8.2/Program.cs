9ccc6066456dbd835fa7296a36b2c4216ae71b4f
Roman-Sykhyi <kariys2015@gmail.com>
2021-10-27 14:50:26 +0300

    Виправлено помилки

--- Задача 8.2/Program.cs
+++ Задача 8.2/Program.cs
@@ -10,43 +10,20 @@ namespace Задача_8._2
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+            Task task = new Task();
+
             Storage storage1 = new Storage();
             Storage storage2 = new Storage();
 
             storage2.products.RemoveAt(1);
             storage1.products.RemoveAt(0);
 
-            PrintProducts(GetCommonProducts(storage1, storage2));
-            PrintProducts(GetProductsLeftJoinIfNull(storage1, storage2));
-            PrintProducts(GetDifferentProducts(storage1, storage2));
+            task.PrintProducts(task.GetCommonProducts(storage1, storage2));
+            task.PrintProducts(task.GetProductsLeftJoinIfNull(storage1, storage2));
+            task.PrintProducts(task.GetDifferentProducts(storage1, storage2));
 
             Console.ReadKey();
         }
-
-        private static List<Product> GetCommonProducts(Storage storage1, Storage storage2)
-        {
-            return storage1.products.Intersect(storage2.products).ToList();
-        }
-
-        private static List<Product> GetProductsLeftJoinIfNull(Storage storage1, Storage storage2)
-        {
-            return storage1.products.Where(p => storage2.products.All(p2 => !p2.Equals(p))).ToList();
-        }
-
-        private static List<Product> GetDifferentProducts(Storage storage1, Storage storage2)
-        {
-            List<Product> result = GetProductsLeftJoinIfNull(storage1, storage2);
-            result.AddRange(GetProductsLeftJoinIfNull(storage2, storage1));
-            return result;
-        }
-
-        private static void PrintProducts(List<Product> products)
-        {
-            for (int i = 0; i < products.Count; i++)
-            {
-                Console.WriteLine(products[i]);
-            }
-            Console.WriteLine();
-        }
+       
     }
 }
---
+++ Задача 8.2/Task.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Задача_8._2
+{
+    public class Task
+    {
+        public IReadOnlyList<Product> GetCommonProducts(Storage storage1, Storage storage2)
+        {
+            return storage1.products.Intersect(storage2.products).ToList().AsReadOnly();
+        }
+
+        public IReadOnlyList<Product> GetProductsLeftJoinIfNull(Storage storage1, Storage storage2)
+        {
+            return storage1.products.Where(p => storage2.products.All(p2 => !p2.Equals(p))).ToList().AsReadOnly();
+        }
+
+        public IReadOnlyList<Product> GetDifferentProducts(Storage storage1, Storage storage2)
+        {
+            IReadOnlyList<Product> result1 = GetProductsLeftJoinIfNull(storage1, storage2);
+            IReadOnlyList<Product> result2 =GetProductsLeftJoinIfNull(storage2, storage1);
+
+            return result1.Concat(result2).ToList().AsReadOnly();
+        }
+
+        public void PrintProducts(IReadOnlyList<Product> products)
+        {
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine(products[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
